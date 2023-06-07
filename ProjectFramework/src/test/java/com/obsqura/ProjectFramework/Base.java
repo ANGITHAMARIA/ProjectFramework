e2add855a0f10f6f66952ec64396fd7cda4dc494
angit <angit@Chikkus_hp.home>
2023-06-06 20:33:48 -0400

    Login page and Base class committed

---
+++ ProjectFramework/src/test/java/com/obsqura/ProjectFramework/Base.java
@@ -0,0 +1,32 @@
+package com.obsqura.ProjectFramework;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class Base {
+
+	
+		public WebDriver driver;
+		
+		@BeforeMethod
+	    public void initializeBrowser()
+	    {
+	        driver =new ChromeDriver();
+	        
+	        driver.get("https://groceryapp.uniqassosiates.com/admin/login");
+	        driver.manage().window().maximize();
+	    }
+	    public void driverClose()
+	    {
+	        driver.close();
+	    }
+	    
+	 //   @AfterMethod
+	    public void driverQuit()
+	    {
+	        driver.quit();
+	    }
+	   
+}
